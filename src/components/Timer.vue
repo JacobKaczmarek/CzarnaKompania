fa1b66a938e82aa4c8d6e581189f850dbf8277aa
Jacob <kuba300698@gmail.com>
2019-04-12 01:00:13 +0200

    Add training view

---
+++ src/components/Timer.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="timerWrapper">
+    <h2>Timer</h2>
+    <div class="innerWrapper">
+      Time:
+      <input type="number" v-model="startTime" placeholder="Time in seconds">
+      <button @click="startTimer()" class="startButton">Start</button>
+      <p v-if="started" class="counter">{{ timeLeft.toFixed(2) }}s</p>
+      <div class="progress-bar-outline">
+        <div class="progress-bar-fill"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { stat } from "fs";
+import { setInterval, clearInterval } from "timers";
+
+export default {
+  data() {
+    return {
+      started: false,
+      startTime: null,
+      timeLeft: null
+    };
+  },
+  methods: {
+    startTimer: function() {
+      this.started = true;
+      this.timeLeft = this.startTime;
+      let progress = 0;
+      let interval = setInterval(() => {
+        this.timeLeft -= 0.01;
+        progress = ((this.startTime - this.timeLeft) / this.startTime) * 100;
+        console.log(progress);
+        $(".progress-bar-fill").css("width", progress + "%");
+        if (this.timeLeft <= 0) {
+          clearInterval(interval);
+          this.timeLeft = 0;
+        }
+      }, 10);
+    }
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+.timerWrapper {
+  margin: 10px;
+  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+input {
+  display: inline;
+  border: none;
+  border-bottom: 2px solid #2c3e50;
+}
+
+input:focus {
+  outline-width: 0;
+}
+
+.startButton {
+  padding: 10px 20px;
+  background: #2c3e50;
+  border: none;
+  border-radius: 3px;
+  color: white;
+  margin: 10px;
+}
+
+.progress-bar-outline {
+  padding: 0;
+  width: 300px;
+  height: 40px;
+  border: 3px solid #2c3e50;
+  border-radius: 5px;
+}
+
+.progress-bar-fill {
+  position: relative;
+  width: 0;
+  margin: 0px;
+  height: 100%;
+  background: #2c3e50;
+  border-radius: 1px;
+}
+</style>
+
---
+++ src/views/Training.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="trainingWrapper">
+    <h1>Training</h1>
+    <h3>Drill for today</h3>
+    <div class="innerWrapper">
+      <img :src="drillPath" alt="drill">
+      <Timer/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Timer from "@/components/Timer";
+
+export default {
+  components: { Timer },
+  data() {
+    return {
+      drillNumber: null,
+      drillPath: ""
+    };
+  },
+  created() {
+    this.drillNumber = Math.ceil(Math.random() * 3);
+    this.drillPath = require("../assets/drills/drill_" +
+      this.drillNumber +
+      ".png");
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.trainingWrapper {
+  margin: 10px;
+
+  img {
+    width: 100%;
+  }
+
+  @media (min-width: 768px) {
+    margin: 30px;
+
+    img {
+      width: 60%;
+      height: auto;
+    }
+
+    .innerWrapper {
+      display: flex;
+      justify-content: space-evenly;
+    }
+  }
+}
+</style>
+
